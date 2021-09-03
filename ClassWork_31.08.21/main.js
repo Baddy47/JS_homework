8d78353ba6c5710f3356523803bbb9827a79e394
Andrei Dziahel <dziahelandrei@gmail.com>
2021-09-03 14:48:47 +0300

    Update main.js

---
+++ ClassWork_31.08.21/main.js
@@ -0,0 +1,31 @@
+const Filter = function() {
+
+    let items = document.querySelectorAll('.galery__item');
+    items.forEach(item => {
+        
+        return item;
+    })
+    console.log(items);
+    let buttons = document.querySelectorAll('.btn');
+    buttons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            
+            if (event.target.dataset.set == 'all') {
+                items.classList.add('active');
+            }
+            if (event.target.dataset.set == 'red') {
+                console.log('red');
+            }
+            if (event.target.dataset.set == 'green') {
+                console.log('green');
+            }
+            if (event.target.dataset.set == 'blue') {
+                console.log('blue');
+            }
+        })
+    })
+
+    
+};
+
+let filter = new Filter();
